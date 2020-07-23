dddaf70d93dc0ca280fadfa728f41b0644ace4b7
Satoru SATOH <satoru.satoh@gmail.com>
2020-07-23 15:53:57 +0900

    enhancement: make ansible-lint find custom rules from rules/custom/*/
    Make ansible-lint to find custom rules in rules/custom/*/ automatically
    by default. Found custom rules will have higher priority than the
    default rules.
    
    This is an alternative way to accomplish the issue #755.

--- lib/ansiblelint/constants.py
+++ lib/ansiblelint/constants.py
@@ -3,6 +3,7 @@ import os.path
 
 
 DEFAULT_RULESDIR = os.path.join(os.path.dirname(__file__), 'rules')
+DEFAULT_CUSTOM_RULESDIR = os.path.join(DEFAULT_RULESDIR, "custom")
 
 INVALID_CONFIG_RC = 2
 ANSIBLE_FAILURE_RC = 3
